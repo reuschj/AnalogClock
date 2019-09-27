55675f49fbe017beff6243390bc8e1dcb060825c
reuschj <reuschj@gmail.com>
2019-09-26 23:42:49 -0500

    Initial Commit

---
+++ AnalogClock/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AnalogClock
+//
+//  Created by Justin Reusch on 9/26/19.
+//  Copyright © 2019 Justin Reusch. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
